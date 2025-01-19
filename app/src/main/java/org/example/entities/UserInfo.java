aa150e138a9dc769c9118c16780887cbfa9c2a1a
AbhishekPSingh07 <abhisheksinghas1205@gmail.com>
2025-01-19 23:59:44 +0530

    initialised entities for auth service

---
+++ app/src/main/java/org/example/entities/UserInfo.java
@@ -0,0 +1,36 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@ToString
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserInfo {
+
+    @Id
+    @Column(name = "user_id")
+    private String user_id;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+
+    private Set<UserRoles> roles = new HashSet<>();
+}
