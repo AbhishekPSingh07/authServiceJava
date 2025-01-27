4114a435f6d4d19edb3d818e4fb5735140676b28
AbhishekPSingh07 <abhisheksinghas1205@gmail.com>
2025-01-27 22:47:32 +0530

    implementation of remaining services and repositories + Models,Requests and Response

---
+++ app/src/main/java/org/example/service/UserDetailsImpl.java
@@ -0,0 +1,59 @@
+package org.example.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.example.entities.UserInfo;
+import org.example.models.UserDto;
+import org.example.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.UUID;
+
+@Component
+@AllArgsConstructor
+@Data
+public class UserDetailsImpl implements UserDetailsService
+{
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserInfo user = userRepository.findByUsername(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("could not find the user");
+        }
+
+        return new CustomUserDetails(user);
+    }
+
+    public UserInfo checkIfUserAlreadyExists(UserDto userInfoDto) {
+        return userRepository.findByUsername(userInfoDto.getUsername());
+    }
+
+    public Boolean signUpUser(UserDto userInfoDto) {
+        //TODO:
+        // Define a function to check if userEmail, password format is correct
+        // do it by validation class utility
+        userInfoDto.setPassword(passwordEncoder.encode(userInfoDto.getPassword()));
+        if(Objects.nonNull((checkIfUserAlreadyExists(userInfoDto)))) {
+            return false;
+        }
+        String userId = UUID.randomUUID().toString();
+        userRepository.save(new UserInfo(userId,userInfoDto.getUsername(),
+                userInfoDto.getPassword(),new HashSet<>()));
+        return true;
+    }
+}
